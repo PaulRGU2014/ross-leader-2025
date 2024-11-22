1af9b17898fab5710395e2b23f4ecaa24cc38f91
Paul Thanataweenont <paul@chronos.email>
2024-11-21 22:02:07 -0700

    update stage site

--- frontend/src/middleware.ts
+++ frontend/src/middleware.ts
@@ -1,11 +1,48 @@
 import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
 
+const [AUTH_USER, AUTH_PASS] = (process.env.HTTP_BASIC_AUTH || ':').split(':');
+
+// Step 2. Check HTTP Basic Auth header if present
+function isAuthenticated(req: NextRequest) {
+  const authheader = req.headers.get('authorization') || req.headers.get('Authorization');
+
+  if (!authheader) {
+    return false;
+  }
+
+  const auth = Buffer.from(authheader.split(' ')[1], 'base64').toString().split(':');
+  const user = auth[0];
+  const pass = auth[1];
+
+  if (user == AUTH_USER && pass == AUTH_PASS) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
 export function middleware(request: NextRequest) {
   const pathname = request.nextUrl.pathname;
 
   const response = NextResponse.next();
   response.headers.set('x-pathname', pathname);
+  // Step 1. HTTP Basic Auth Middleware for Challenge
+  if (!isAuthenticated(request)) {
+    return new NextResponse('Authentication required', {
+      status: 401,
+      headers: { 'WWW-Authenticate': 'Basic' },
+    });
+  }
 
   return response;
 }
+
+
+
+
+
+// // Step 3. Configure "Matching Paths" below to protect routes with HTTP Basic Auth
+// export const config = {
+//   matcher: '/:path*',
+// };
