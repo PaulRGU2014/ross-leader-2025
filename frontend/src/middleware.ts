7e7fb2b23a42617a3e744fe253bf9a4820034e35
Paul Thanataweenont <paul@chronos.email>
2024-12-07 21:23:00 -0700

    update stage

--- frontend/src/middleware.ts
+++ frontend/src/middleware.ts
@@ -1,5 +1,5 @@
-import { NextResponse } from 'next/server';
-import type { NextRequest } from 'next/server';
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
 
 const [AUTH_USER, AUTH_PASS] = ["R0S5", "R0S5"];
 
@@ -7,42 +7,42 @@ export function middleware(request: NextRequest) {
   const pathname = request.nextUrl.pathname;
 
   const response = NextResponse.next();
-  response.headers.set('x-pathname', pathname);
+  response.headers.set("x-pathname", pathname);
 
   // Step 1. HTTP Basic Auth Middleware for Challenge
   if (!isAuthenticated(request)) {
-    return new NextResponse('Authentication required', {
+    return new NextResponse("Authentication required", {
       status: 401,
-      headers: { 'WWW-Authenticate': 'Basic' },
+      headers: { "WWW-Authenticate": "Basic" },
     });
   }
 
   // Step 2. Check HTTP Basic Auth header if present
   function isAuthenticated(req: NextRequest) {
-  const authheader = req.headers.get('authorization') || req.headers.get('Authorization');
+    const authheader =
+      req.headers.get("authorization") || req.headers.get("Authorization");
 
-  if (!authheader) {
-    return false;
+    if (!authheader) {
+      return false;
+    }
+
+    const auth = Buffer.from(authheader.split(" ")[1], "base64")
+      .toString()
+      .split(":");
+    const user = auth[0];
+    const pass = auth[1];
+
+    if (user === AUTH_USER && pass === AUTH_PASS) {
+      return true;
+    } else {
+      return false;
+    }
   }
-
-  const auth = Buffer.from(authheader.split(' ')[1], 'base64').toString().split(':');
-  const user = auth[0];
-  const pass = auth[1];
-
-
-  if (user === AUTH_USER && pass === AUTH_PASS) {
-    return true;
-  } else {
-    return false;
-  }
-}
 
   return response;
 }
 
 // Step 3. Configure "Matching Paths" below to protect routes with HTTP Basic Auth
 export const config = {
-  matcher: '/:path*',
+  matcher: "/:path*",
 };
-
-//  test
