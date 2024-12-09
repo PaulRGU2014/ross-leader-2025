0cf132cc1116f02291ebf3cb9636029b659b7005
Paul Thanataweenont <paul@chronos.email>
2024-12-08 18:32:25 -0700

    update Full Page hero

--- frontend/src/components/FullPageHero/FullPageHero.tsx
+++ frontend/src/components/FullPageHero/FullPageHero.tsx
@@ -1,15 +1,43 @@
 "use client"
 
 import styles from './FullPageHero.module.scss';
+import ReactPlayer from 'react-player'
+import InViewAnim from './../../utils/InViewAnim/InViewAnim'
+import { useState, useEffect } from 'react'
 
 interface FullPageHeroProps {
   content: any; // Replace 'any' with the appropriate type
 }
 
 export default function FullPageHero({ content }: FullPageHeroProps) {
+  const [isClient, setIsClient] = useState(false)
+
+  useEffect(() => {
+    setIsClient(true)
+  }, [])
+
+  if (!isClient) {
+    return null
+  }
+
   return(
-    <div className={styles.component}>
-      <h5>FullPageHero</h5>
-    </div>
+    <InViewAnim><div className={styles.component}>
+      <ReactPlayer 
+        className={styles.reactPlayer}
+        url={content.media_url}
+        loop={true}
+        muted={true}
+        playing={true}
+        playsinline={true}
+        width='100%'
+        height='100%'
+      />
+      <div className={styles.wrapper}>
+        <div className={styles.content}>
+          <h4>{content.title}</h4>
+          <h5>{content.subtitle}</h5>
+        </div>
+      </div>      
+    </div></InViewAnim>
   );
 }
--- frontend/src/sanity/schemaTypes/fullPageHero.ts
+++ frontend/src/sanity/schemaTypes/fullPageHero.ts
@@ -9,5 +9,61 @@ export const fullPageHero = defineType({
       name: 'title',
       type: 'string',
     }),
+    defineField({
+      name: 'subtitle',
+      title: 'Subtitle',
+      type: 'string',
+    }),
+    defineField({
+      name: 'media_source',
+      title: 'Media Source',
+      type: 'string',
+      options: {
+        list: [
+          {title: 'URL', value: 'url'},
+          {title: 'File', value: 'file'}
+        ]
+      }
+    }),
+    defineField({
+      name: 'media_url',
+      title: 'Media URL',
+      type: 'url',
+      hidden: ({parent}) => parent?.media_source !== 'url',
+      validation: Rule => Rule.uri({
+        scheme: ['http', 'https', 'mailto', 'tel']
+      })
+    }),
+    defineField({
+      name: 'media_type',
+      title: 'Media Type',
+      type: 'string',
+      options: {
+        list: [
+          {title: 'Image', value: 'image'},
+          {title: 'Video', value: 'video'}
+        ]
+      },
+      hidden: ({parent}) => !parent?.media_source,
+    }),
+    defineField({
+      name: 'image_file',
+      title: 'Image File',
+      type: 'image',
+      fields: [
+        {
+          name: 'alt',
+          title: 'Image Alt Text',
+          type: 'string',
+        },
+      ],
+      hidden: ({parent}) => parent?.media_source !== 'file' || parent?.media_type !== 'image',
+    }),
+    defineField({
+      name: 'video_file',
+      title: 'Video File',
+      type: 'file',
+      hidden: ({parent}) => parent?.media_source !== 'file' || parent?.media_type !== 'video',
+    }),
   ],
 })
