a55744158f6ba7e537fe7a3dcd9a8436806efa3b
Paul Thanataweenont <paul@chronos.email>
2024-12-08 21:56:43 -0700

    Metadata and Menu

--- frontend/src/app/[slug]/page.tsx
+++ frontend/src/app/[slug]/page.tsx
@@ -7,17 +7,36 @@ import Header from '@/components/Header/Header'
 import Footer from '@/components/Footer/Footer'
 import { headers } from 'next/headers'
 import { GoogleAnalytics } from '@next/third-parties/google'
+import type { Metadata } from 'next'
 
+export async function generateMetadata(): Promise<Metadata> {
+  const headersList = await headers();
+  const pathname = headersList.get('x-pathname') || '';
+  
+  const query = `*[_type=="pages"&& page_url.current=="${pathname}"]{...,components[]->}`;
+  const data = await client.fetch(query);
+
+  if (!data || data.length === 0) {
+    return {
+      title: 'Default Title', // Fallback title
+    };
+  }
+
+  return {
+    title: data[0].page_title,
+  };
+}
 
 export default async function Page() {
   const headersList = await headers();
   const pathname = headersList.get('x-pathname') || '';
-
+  
   const query = `*[_type=="pages"&& page_url.current=="${pathname}"]{...,components[]->}`;
   const footerQuery = `*[_type=="footer"]{...}`;
   const data = await client.fetch(query);
   const footerData = await client.fetch(footerQuery);
   const menuData = await client.fetch(`*[_type=="header"]{...}`);
+  
 
   if (!data) {
     return <div className='loading'>Loading...</div>;
