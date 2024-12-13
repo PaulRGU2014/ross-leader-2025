e6fa5c1628f763e32c58a0217db23eb77c977a98
Paul Thanataweenont <paul@chronos.email>
2024-12-12 19:28:50 -0700

    backup

--- frontend/src/components/Footer/Footer.tsx
+++ frontend/src/components/Footer/Footer.tsx
@@ -1,7 +1,6 @@
-"use client"
 
-import { useEffect, useState } from "react";
-import Link from 'next/link';
+// import { useEffect, useState } from "react";
+import Link from '@/utils/LinkWrapper/LinkWrapper';
 import styles from './Footer.module.scss';
 import InViewAnim from './../../utils/InViewAnim/InViewAnim'
 
@@ -9,19 +8,20 @@ import InViewAnim from './../../utils/InViewAnim/InViewAnim'
 // get the content from sanity and pass it as props
 
 
-export default function Footer({ content, pathname }: { content?: any, pathname: string }) {
+export default function Footer({ content }: { content?: any }) {
 
-    const currentYear = new Date().getFullYear()
+  const currentYear = new Date().getFullYear()
 
 
-    return (
-        <InViewAnim><div className={styles.component}>
-            <div className={styles.wrapper}>
-                <div className={styles.inner}>
-
-                    <p>© {currentYear} ROSS Leaders. All Rights Reserved.</p>
-                </div>
-            </div>
-        </div></InViewAnim>
-    )
+  return (
+    <InViewAnim><div className={styles.component}>
+      <div className={styles.wrapper}>
+        <div className={styles.disclaimer}>
+          <p>© {currentYear} ROSS Leaders. All Rights Reserved.</p>
+          <p>ROSS Leaders is a 501(c)(3) nonprofit recognized by the IRS, and all donations to ROSS Leaders are tax deductible in accordance with IRS regulations.</p>
+          <p>ROSS Leaders follows a policy of non-discrimination in accordance with the law.</p>
+        </div>
+      </div>
+    </div></InViewAnim>
+  )
 }
