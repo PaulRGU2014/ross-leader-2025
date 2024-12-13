66b43cdbda0d83b354353d5f2f2ed1fd4cbb059a
Paul Thanataweenont <paul@chronos.email>
2024-12-12 23:29:56 -0700

    update footer field

--- frontend/src/sanity/schemaTypes/footer.ts
+++ frontend/src/sanity/schemaTypes/footer.ts
@@ -10,6 +10,21 @@ export const footer = defineType({
       type: 'string',
     }),
     defineField({
+      name: 'contact_phone',
+      title: 'Phone Number',
+      type: 'string',
+    }),
+    defineField({
+      name: 'contact_email',
+      title: 'Email',
+      type: 'string',
+    }),
+    defineField({
+      name: 'contact_address',
+      type: 'text',
+      title: 'Address',
+    }),
+    defineField({
       name: 'footer_links',
       type: 'array',
       of: [
