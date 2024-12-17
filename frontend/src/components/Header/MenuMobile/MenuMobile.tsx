4a702ce62205da9fffe7906aa6f746a979a63c2f
Paul Thanataweenont <paul@chronos.email>
2024-12-17 02:03:35 -0700

    update format + css

--- frontend/src/components/Header/MenuMobile/MenuMobile.tsx
+++ frontend/src/components/Header/MenuMobile/MenuMobile.tsx
@@ -22,9 +22,9 @@ function Hamburger({ isMenuOpen, hamburgerRef, onClick, initialLoad }: { isMenuO
         }
       }}
     >
-      <div className={styles.hamburger_first}/>
+      <div className={styles.hamburger_first} />
       <div className={styles.hamburger_second} />
-      <div className={styles.hamburger_third}/>
+      <div className={styles.hamburger_third} />
     </div>
   );
 }
@@ -40,49 +40,89 @@ interface MenuContentProps {
   handleMenuClick: (event: React.MouseEvent<HTMLLIElement | HTMLAnchorElement>, index: number, url: string, hasSubMenus: boolean) => void;
 }
 
-function MenuContent({ 
-  content, 
-  isMenuOpen, 
-  setIsMenuOpen, 
-  isMenuOpening, 
+function MenuContent({
+  content,
+  isMenuOpen,
+  setIsMenuOpen,
+  isMenuOpening,
   menuRef,
   mainMenuIndex,
   setMainMenuIndex,
-  handleMenuClick 
+  handleMenuClick
 }: MenuContentProps) {
+  const [hoveredMenuIndex, setHoveredMenuIndex] = useState<number | null>(null);
+
   return (
     <div className={isMenuOpen ? styles.inner : styles.inner_close}>
       <ul className={styles.content} ref={menuRef}>
         <Link href="/" className={styles.logo_main}>
-          <Image 
-            src="/Logo_Menu.png" 
-            alt="Logo" 
-            width={120} 
-            height={120} 
+          <Image
+            src="/Logo_Menu.png"
+            alt="Logo"
+            width={120}
+            height={120}
             priority={true}
-            style={{objectPosition: "center"}} 
+            style={{ objectPosition: "center" }}
           />
-        </Link>          
+        </Link>
         {content?.menu_list?.map((item: any, index: number) => (
-          <li 
-            className={`${styles.link} ${isMenuOpen === true ? styles.open : ""}`} 
-            key={index} 
+          <li
+            className={`${styles.link} ${isMenuOpen === true ? styles.open : ""}`}
+            key={index}
             style={{ transitionDelay: isMenuOpening ? `${(index + 1) * 100}ms` : "0ms" }}
             onMouseEnter={() => setMainMenuIndex(index)}
             onMouseLeave={() => setMainMenuIndex(-1)}
             onClick={(event) => handleMenuClick(event, index, item.url, !!item.sub_menus_1)}
           >
-            <Link 
+            <Link
               href={item.url}
               onClick={(event) => handleMenuClick(event, index, item.url, !!item.sub_menus_1)}
             >
-            {item.title}{item.sub_menus_1 && item.sub_menus_1.length > 0 && <div className={`${styles.dropdown} ${mainMenuIndex === index ? styles.active : ''}`}><BsChevronCompactDown /></div>}
+              {item.title}{item.sub_menus_1 && item.sub_menus_1.length > 0 && <div className={`${styles.dropdown} ${mainMenuIndex === index ? styles.active : ''}`}><BsChevronCompactDown /></div>}
             </Link>
+            {item?.sub_menus_1 && (
+              <ul className={`${styles.subMenu_wrapper} ${mainMenuIndex === index ? styles.active : ''}`}>
+                {item?.sub_menus_1?.map((subMenus1: any, subIndex: number) => (
+                  <li 
+                    key={subIndex} 
+                    className={`${styles.subMenu}`}
+                    onMouseEnter={() => setHoveredMenuIndex(index)}
+                    onMouseLeave={() => setHoveredMenuIndex(null)}
+                    onClick={(event) => handleMenuClick(event, index, subMenus1.url, !!subMenus1.sub_menus_2)}
+                  >
+                    <Link 
+                      href={subMenus1.url} 
+                      className={styles.subMenu_link}
+                    >
+                      {subMenus1.title}
+                      {subMenus1.sub_menus_2 && subMenus1.sub_menus_2.length > 0 && <div className={`${styles.dropdown} ${hoveredMenuIndex === index ? styles.active : ''}`}><BsChevronCompactDown /></div>}
+                    </Link>
+                    {subMenus1?.sub_menus_2 && (
+                      <ul className={`${styles.subMenu_wrapper} ${hoveredMenuIndex === index ? styles.active : ''}`}>
+                        {subMenus1?.sub_menus_2?.map((subMenus2: any, subIndex2: number) => (
+                          <li 
+                            key={subIndex2} 
+                            className={styles.subMenu}
+                          >
+                            <Link 
+                              href={subMenus2.url} 
+                              className={styles.subMenu_link}
+                            >
+                              {subMenus2.title}
+                            </Link>
+                          </li>
+                        ))}
+                      </ul>
+                    )}
+                  </li>
+                ))}
+              </ul>
+            )}
           </li>
         ))}
-        <li 
-          className={styles.menuButton_wrapper} 
-          style={{ 
+        <li
+          className={styles.menuButton_wrapper}
+          style={{
             transitionDelay: isMenuOpening ? `${(content.menu_list?.length + 4) * 100}ms` : "0ms",
             transform: isMenuOpen ? "translateY(0px)" : "translateY(20px)",
             opacity: isMenuOpen ? 1 : 0
@@ -133,7 +173,7 @@ export default function MenuMobile({ content, handleMenuClick }: MenuMobileProps
 
   const menuListNumber = content?.footer_links?.length || 0;
 
-  function handleMenuToggle () {
+  function handleMenuToggle() {
     if (!isMenuOpen) {
       setIsMenuOpening(true);
       setIsMenuOpen(true);
@@ -146,14 +186,14 @@ export default function MenuMobile({ content, handleMenuClick }: MenuMobileProps
   };
 
   return (
-    <div 
+    <div
       className={`${styles.component} ${!isMenuOpen ? styles.component_close : ""}`}
     >
-      <div 
+      <div
         className={`${styles.wrapper} ${isMenuOpen ? styles.open : ""}`}
       >
         <Hamburger isMenuOpen={isMenuOpen} hamburgerRef={hamburgerRef} onClick={handleMenuToggle} initialLoad={initialLoad} />
-        <MenuContent {...{content, isMenuOpen, setIsMenuOpen, isMenuOpening, menuRef, mainMenuIndex, setMainMenuIndex, handleMenuClick}} />
+        <MenuContent {...{ content, isMenuOpen, setIsMenuOpen, isMenuOpening, menuRef, mainMenuIndex, setMainMenuIndex, handleMenuClick }} />
       </div>
     </div>
   );
