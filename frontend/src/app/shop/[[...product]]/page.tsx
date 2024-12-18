4e899676411663c42d343e7b9cec3ade91f3ed28
Paul Thanataweenont <paul@chronos.email>
2024-12-17 21:17:04 -0700

    start e-commerce

--- frontend/src/app/shop/[[...product]]/page.tsx
+++ frontend/src/app/shop/[[...product]]/page.tsx
@@ -1,6 +1,5 @@
 import '../../../scss/global.scss'
 import client from "../../../../client"
-import { headers } from 'next/headers'
 import Header from '@/components/Header/Header'
 import Footer from '@/components/Footer/Footer'
 import type { Metadata } from 'next'
@@ -8,10 +7,8 @@ import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
 export async function generateMetadata(): Promise<Metadata> {
-  const headersList = await headers();
-  const pathname = headersList.get('x-pathname') || '';
   
-  const query = `*[_type=="pages"&& page_url.current=="${pathname}"]{...,components[]->}`;
+  const query = `*[_type=="pages"&& page_url.current=="/shop"]{...,components[]->}`;
   const data = await client.fetch(query);
 
   if (!data || data.length === 0) {
@@ -26,9 +23,7 @@ export async function generateMetadata(): Promise<Metadata> {
 }
 
 export default async function Page() {
-  const headersList = await headers();
-  const pathname = headersList.get('x-pathname') || '';
-  const footerQuery = `*[_type=="footer"]{...}`;
+  const footerQuery = `*[_type=="pages"&& page_url.current=="/shop"]{footer->}`;
   const footerData = await client.fetch(footerQuery);
   const menuData = await client.fetch(`*[_type=="header"]{...}`);
   console.log('footerData', footerData);
