dfc6a075a0dec95391521695c2a2993c30ce9b2a
Paul Thanataweenont <paul@chronos.email>
2024-12-22 13:38:48 -0700

    update new component

--- frontend/src/components/FullPageZoom/FullPageZoom.tsx
+++ frontend/src/components/FullPageZoom/FullPageZoom.tsx
@@ -1,44 +1,60 @@
-"use client"
+"use client";
 
+import { useEffect, useRef, useState } from 'react';
+import { gsap } from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
+import ReactPlayer from 'react-player';
+import InViewAnim from './../../utils/InViewAnim/InViewAnim';
 import styles from './FullPageZoom.module.scss';
-import InViewAnim from './../../utils/InViewAnim/InViewAnim'
-import { gsap } from "gsap";
-import { ScrollTrigger } from "gsap/ScrollTrigger";
-import ReactPlayer from 'react-player'
-import { useState, useEffect } from 'react'
-
 
 gsap.registerPlugin(ScrollTrigger);
 
-interface FullPageZoomProps {
-  content: any; // Replace 'any' with the appropriate type
-}
-
-export default function FullPageZoom({content}: FullPageZoomProps) {
-  const [isClient, setIsClient] = useState(false)
+export default function FullPageZoom({ content }: { content: any }) {
+  const mediaRef = useRef<HTMLDivElement>(null);
+  const [isClient, setIsClient] = useState(false);
 
   useEffect(() => {
-    setIsClient(true)
-  }, [])
+    setIsClient(true);
+  }, []);
+
+  useEffect(() => {
+    if (isClient && mediaRef.current) {
+      gsap.fromTo(
+        mediaRef.current,
+        { scale: 0.3 },
+        {
+          scale: 1,
+          scrollTrigger: {
+            trigger: mediaRef.current,
+            start: 'top+=50% bottom',
+            end: 'top top',
+            scrub: true,
+          },
+        }
+      );
+    }
+  }, [isClient]);
 
   if (!isClient) {
-    return null
+    return null;
   }
 
-  return(
-    <InViewAnim><div className={styles.component}>
-      <div className={styles.media}>
-        <ReactPlayer 
-          className={styles.reactPlayer}
-          url={content.media_url}
-          loop={true}
-          muted={true}
-          playing={true}
-          playsinline={true}
-          width='177vh'
-          height='100%'
-        />
-      </div>    
-    </div></InViewAnim>
+  return (
+    <InViewAnim>
+      <div className={styles.component}>
+        <div className={styles.media} ref={mediaRef}>
+          <ReactPlayer
+            className={styles.reactPlayer}
+            url={content.media_url}
+            loop={true}
+            muted={true}
+            playing={true}
+            playsinline={true}
+            width='177vh'
+            height='100%'
+          />
+        </div>
+      </div>
+    </InViewAnim>
   );
 }
