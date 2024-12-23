381ac9ec82db59a28941bc599d3621fa65912610
Paul Thanataweenont <paul@chronos.email>
2024-12-22 23:45:51 -0700

    backup

--- frontend/src/components/HeroBannerImg/HeroBannerImg.tsx
+++ frontend/src/components/HeroBannerImg/HeroBannerImg.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { gsap } from 'gsap';
 import { ScrollTrigger } from 'gsap/ScrollTrigger';
 import styles from './HeroBannerImg.module.scss';
@@ -14,22 +14,39 @@ interface HeroBannerImgProps {
 }
 
 export default function HeroBannerImg({ content }: HeroBannerImgProps) {
+  const [isClient, setIsClient] = useState(false);
+  const [isActive, setIsActive] = useState(false);
+
   useEffect(() => {
-    gsap.fromTo(
-      `.${styles.image}`,
-      { y: 300, opacity: 0 },
-      {
-        y: 0,
-        opacity: 1,
-        scrollTrigger: {
-          trigger: `.${styles.component}`,
-          start: 'top 50%',
-          end: 'top top',
-          scrub: true,
-        },
-      }
-    );
+    setIsClient(true);
   }, []);
+
+  useEffect(() => {
+    if (isClient) {
+      gsap.fromTo(
+        `.${styles.image}`,
+        { y: 300, opacity: 0 },
+        {
+          y: 0,
+          opacity: 1,
+          scrollTrigger: {
+            trigger: `.${styles.component}`,
+            start: 'top top+=60%',
+            end: 'top top+=10%',
+            scrub: true,
+            onEnter: () => setIsActive(false),
+            onLeave: () => setIsActive(true),
+            onEnterBack: () => setIsActive(false),
+            onLeaveBack: () => setIsActive(true),
+          },
+        }
+      );
+    }
+  }, [isClient]);
+
+  if (!isClient) {
+    return null;
+  }
 
   return (
     <InViewAnim>
@@ -44,8 +61,8 @@ export default function HeroBannerImg({ content }: HeroBannerImgProps) {
           priority={true}
         />
         <div className={styles.wrapper}>
-          <h3>{content.title}</h3>
-          <h5>{content.subtitle}</h5>
+          {content.title && <h3 className={`${styles.title} ${!!isActive ? styles.active : ""}`}>{content.title}</h3>}
+          {content.subtitle && <h5 className={`${styles.subtitle} ${!!isActive ? styles.active : ""}`}>{content.subtitle}</h5>}
         </div>
       </div>
     </InViewAnim>
